427d54375b29d5a9e1f0dc61aaeeb4725c198625
Thibe Provost <thibe.provost@viso-cor-mariae.be>
2022-04-25 08:49:07 +0200

    Add project files.

---
+++ datetime 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace datetime_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Jaar;
+            Console.WriteLine("geef een jaar op:");
+            Jaar = Convert.ToInt32(Console.ReadLine());
+
+
+
+            if (DateTime.IsLeapYear(Jaar)== true)
+            {
+                Console.WriteLine($"{Jaar} is een schrikkeljaar.");
+            }
+            else
+            {
+                Console.WriteLine($"{Jaar} is geen schrikkeljaar.");
+            }
+        }
+    }
+}
